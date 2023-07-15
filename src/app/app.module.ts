a02a2baa31c95b57737c1db9d5fd80faf2111812
Mafadi Lephaka <mafadilephaka97@gmail.com>
2023-07-15 19:33:13 +0200

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomepageComponent } from './homepage/homepage.component';
+import { ContactComponent } from './contact/contact.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { AboutComponent } from './about/about.component';
 
 @NgModule({
-  declarations: [	
+  declarations: [					
     AppComponent,
-      HomepageComponent
+      HomepageComponent,
+      ContactComponent,
+      NavbarComponent,
+      FooterComponent,
+      AboutComponent
    ],
   imports: [
     BrowserModule,
